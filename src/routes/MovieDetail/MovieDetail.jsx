f593ba88fb22520d29865fbc2a039647da49b495
Juan Nebbia <juan.nebbia@gmail.com>
2024-12-11 18:44:14 -0300

    Fix images routes and move static files to public

--- src/routes/MovieDetail/MovieDetail.jsx
+++ src/routes/MovieDetail/MovieDetail.jsx
@@ -35,7 +35,7 @@ const MovieDetail = () => {
                 <div className='movie-detail-container'>
                     <h2>{movieData?.title}</h2>
                     <div className="movie-detail-inner">
-                        <img src={new URL(movieData.poster, import.meta.url).href} alt={movieData?.title} />
+                        <img src={movieData?.poster} alt={movieData?.title} />
                         <div className="detail-data">
                             <div className="genres">{movieData.genres.map(genre => genre).join(', ')}</div>
                             <p className='movie-detail-summary'>{movieData.summary}</p>
